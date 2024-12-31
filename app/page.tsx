a326887d1ac389ccdca97aa83f5850b144a015b3
Kamal Yadav <kamalyadavand@gmail.com>
2024-12-31 13:39:03 +0530

    "second"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import Button from "./components/Elements Section/Button";
+
 export default function Home() {
   return (
     <div className="p-8">
       <h2 className="text-center w-full text-3xl border-gray-50">THIS IS HOMEPAGE</h2>
+      <div className="w-lg p-5">
+        <Link href={`/blogs`} className="w-full">
+          <Button text="GO TO THE ALL BLOGS PAGE" />
+        </Link>
+      </div>
     </div>
   );
 }
